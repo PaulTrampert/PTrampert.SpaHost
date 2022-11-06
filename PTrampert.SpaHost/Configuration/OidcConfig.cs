06ab6f26c27177ad936c0fb5194a238a1f688291
Paul Trampert <paul.trampert@ptrampert.com>
2022-11-06 00:54:45 -0400

    Added auth support for OpenId Connect.

---
+++ PTrampert.SpaHost/Configuration/OidcConfig.cs
@@ -0,0 +1,10 @@
+﻿namespace PTrampert.SpaHost.Configuration
+{
+    public class OidcConfig
+    {
+        public string Authority { get; set; }
+        public string ClientId { get; set; }
+        public string ClientSecret { get; set; }
+        public IEnumerable<string> Scopes { get; set; }
+    }
+}
--- PTrampert.SpaHost/Program.cs
+++ PTrampert.SpaHost/Program.cs
@@ -1,34 +1,76 @@
 
 using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Authentication.OpenIdConnect;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.Extensions.FileProviders;
+using Microsoft.IdentityModel.Protocols.OpenIdConnect;
+using PTrampert.SpaHost.Configuration;
 
 var builder = WebApplication.CreateBuilder(args);
 var config = builder.Configuration;
 builder.Services.AddApiProxy(builder.Configuration.GetSection("ApiProxy"));
-builder.Services.AddAuthentication(opts =>
+var oidcConfig = config.GetSection("Auth")?.Get<OidcConfig>();
+
+if (oidcConfig != null)
+{
+    builder.Services.AddAuthentication(opts =>
+        {
+            opts.DefaultAuthenticateScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+            opts.DefaultSignInScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+            opts.DefaultChallengeScheme = OpenIdConnectDefaults.AuthenticationScheme;
+        })
+        .AddCookie()
+        .AddOpenIdConnect(opts =>
+        {
+            opts.Authority = oidcConfig.Authority;
+
+            opts.ClientId = oidcConfig.ClientId;
+            opts.ClientSecret = oidcConfig.ClientSecret;
+
+            opts.ResponseType = OpenIdConnectResponseType.CodeIdToken;
+            opts.ResponseMode = OpenIdConnectResponseMode.FormPost;
+
+            opts.Scope.Clear();
+            foreach (var scope in oidcConfig.Scopes)
+            {
+                opts.Scope.Add(scope);
+            }
+
+            opts.SaveTokens = true;
+        });
+
+    builder.Services.AddAuthorization(opts =>
     {
-        opts.DefaultAuthenticateScheme = CookieAuthenticationDefaults.AuthenticationScheme;
-        opts.DefaultChallengeScheme = OpenIdConnectDefaults.AuthenticationScheme;
-    })
-    .AddCookie()
-    .AddOpenIdConnect(opts =>
-    {
-        opts.ClientId = config.GetValue<string>("Auth:ClientId");
-        opts.ClientSecret = config.GetValue<string>("Auth:ClientSecret");
-        opts.Authority = config.GetValue<string>("Auth:Authority");
+        opts.FallbackPolicy = new AuthorizationPolicyBuilder()
+            .RequireAuthenticatedUser()
+            .Build();
     });
+}
 
 var app = builder.Build();
 
+
+
 app.UseHttpLogging();
 
-if (config.GetSection("Authentication") != null)
+app.UseRouting();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseCookiePolicy(new CookiePolicyOptions
+    {
+        MinimumSameSitePolicy = SameSiteMode.Unspecified,
+        OnAppendCookie = opts => opts.CookieOptions.SameSite = SameSiteMode.Unspecified,
+        OnDeleteCookie = opts => opts.CookieOptions.SameSite = SameSiteMode.Unspecified,
+    });
+}
+
+if (oidcConfig != null)
 {
     app.UseAuthentication();
     app.UseAuthorization();
 }
 
-app.UseRouting();
 app.UseStaticFiles();
 app.UseApiProxy();
 

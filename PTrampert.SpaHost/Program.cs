6b9955d846384bc7be9877c93a2cd59ae636d97a
Paul Trampert <paul.trampert@ptrampert.com>
2022-11-11 18:31:15 -0500

    Added serilog and setup default config to use json-formatted logging.

--- PTrampert.SpaHost/Program.cs
+++ PTrampert.SpaHost/Program.cs
@@ -7,93 +7,115 @@ using Microsoft.AspNetCore.DataProtection;
 using System.Text.Json;
 using Mcrio.Configuration.Provider.Docker.Secrets;
 using PTrampert.SpaHost.Configuration;
+using Serilog;
 using StackExchange.Redis;
 
-var builder = WebApplication.CreateBuilder(args);
-builder.Host.ConfigureAppConfiguration((builderCtx, configurationBuilder) =>
+Log.Logger = new LoggerConfiguration()
+    .WriteTo.Console()
+    .CreateBootstrapLogger();
+Log.Information("Starting up");
+
+try
 {
-    var additionalConfigs = Environment.GetEnvironmentVariable("SPAHOST_ADDITIONAL_APPSETTINGS");
-    if (additionalConfigs != null)
+    var builder = WebApplication.CreateBuilder(args);
+    builder.Host.ConfigureAppConfiguration((builderCtx, configurationBuilder) =>
     {
-        foreach (var file in additionalConfigs.Split(",", StringSplitOptions.TrimEntries | StringSplitOptions.RemoveEmptyEntries))
+        var additionalConfigs = Environment.GetEnvironmentVariable("SPAHOST_ADDITIONAL_APPSETTINGS");
+        if (additionalConfigs != null)
         {
-            configurationBuilder.AddJsonFile(file);
+            foreach (var file in additionalConfigs.Split(",",
+                         StringSplitOptions.TrimEntries | StringSplitOptions.RemoveEmptyEntries))
+            {
+                configurationBuilder.AddJsonFile(file);
+            }
+        }
+
+        configurationBuilder.AddDockerSecrets();
+        configurationBuilder.AddEnvironmentVariables();
+        configurationBuilder.AddCommandLine(args);
+    });
+    var config = builder.Configuration;
+
+    builder.Host.UseSerilog((ctx, lc) => 
+        lc.ReadFrom.Configuration(ctx.Configuration));
+
+    var authConfig = config.GetSection("AuthConfig")?.Get<AuthConfig>();
+    var fhConfig = config.GetSection("ForwardedHeaders").Get<ForwardedHeadersConfig>();
+    var redisConfig = config.GetSection("RedisConfig").Get<RedisConfig>();
+
+    JwtSecurityTokenHandler.DefaultInboundClaimTypeMap.Clear();
+
+    builder.Services.AddOptions<JsonSerializerOptions>()
+        .Configure(opts => opts.PropertyNamingPolicy = JsonNamingPolicy.CamelCase);
+    builder.Services.AddApiProxy(builder.Configuration.GetSection("ApiProxy"));
+
+    if (authConfig != null)
+    {
+        if (redisConfig.UseForDataProtection)
+        {
+            var redis = ConnectionMultiplexer.Connect(redisConfig.DataProtectionConnectionString);
+            builder.Services.AddDataProtection()
+                .SetApplicationName(builder.Environment.ApplicationName)
+                .PersistKeysToStackExchangeRedis(redis, $"{builder.Environment.ApplicationName}_dpapi");
+        }
+
+        builder.Services.AddAuthentication(opts =>
+            {
+                opts.DefaultAuthenticateScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+                opts.DefaultSignInScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+                opts.DefaultChallengeScheme = OpenIdConnectDefaults.AuthenticationScheme;
+            })
+            .AddCookie(authConfig.CookieConfig.ConfigureCookies)
+            .AddOpenIdConnect(authConfig.OidcConfig.ConfigureOidc);
+
+        if (authConfig.RequireForStaticFiles)
+        {
+            builder.Services.AddAuthorization(opts =>
+            {
+                opts.FallbackPolicy = new AuthorizationPolicyBuilder()
+                    .RequireAuthenticatedUser()
+                    .Build();
+            });
         }
     }
 
-    configurationBuilder.AddDockerSecrets();
-    configurationBuilder.AddEnvironmentVariables();
-    configurationBuilder.AddCommandLine(args);
-});
-var config = builder.Configuration;
+    builder.Services.Configure<ForwardedHeadersOptions>(fhConfig.ConfigureForwardedHeaders);
 
-var authConfig = config.GetSection("AuthConfig")?.Get<AuthConfig>();
-var fhConfig = config.GetSection("ForwardedHeaders").Get<ForwardedHeadersConfig>();
-var redisConfig = config.GetSection("RedisConfig").Get<RedisConfig>();
+    var app = builder.Build();
 
-JwtSecurityTokenHandler.DefaultInboundClaimTypeMap.Clear();
+    app.UseSerilogRequestLogging();
 
-builder.Services.AddOptions<JsonSerializerOptions>()
-    .Configure(opts => opts.PropertyNamingPolicy = JsonNamingPolicy.CamelCase);
-builder.Services.AddApiProxy(builder.Configuration.GetSection("ApiProxy"));
-
-if (authConfig != null)
-{
-    if (redisConfig.UseForDataProtection)
+    app.UseForwardedHeaders();
+    app.UseRouting();
+    if (app.Environment.IsDevelopment())
     {
-        var redis = ConnectionMultiplexer.Connect(redisConfig.DataProtectionConnectionString);
-        builder.Services.AddDataProtection()
-            .SetApplicationName(builder.Environment.ApplicationName)
-            .PersistKeysToStackExchangeRedis(redis, $"{builder.Environment.ApplicationName}_dpapi");
-    }
-
-    builder.Services.AddAuthentication(opts =>
+        app.UseCookiePolicy(new CookiePolicyOptions
         {
-            opts.DefaultAuthenticateScheme = CookieAuthenticationDefaults.AuthenticationScheme;
-            opts.DefaultSignInScheme = CookieAuthenticationDefaults.AuthenticationScheme;
-            opts.DefaultChallengeScheme = OpenIdConnectDefaults.AuthenticationScheme;
-        })
-        .AddCookie(authConfig.CookieConfig.ConfigureCookies)
-        .AddOpenIdConnect(authConfig.OidcConfig.ConfigureOidc);
-
-    if (authConfig.RequireForStaticFiles)
-    {
-        builder.Services.AddAuthorization(opts =>
-        {
-            opts.FallbackPolicy = new AuthorizationPolicyBuilder()
-                .RequireAuthenticatedUser()
-                .Build();
+            MinimumSameSitePolicy = SameSiteMode.Unspecified,
+            OnAppendCookie = opts => opts.CookieOptions.SameSite = SameSiteMode.Unspecified,
+            OnDeleteCookie = opts => opts.CookieOptions.SameSite = SameSiteMode.Unspecified,
         });
     }
-}
 
-builder.Services.Configure<ForwardedHeadersOptions>(fhConfig.ConfigureForwardedHeaders);
-
-var app = builder.Build();
-
-app.UseHttpLogging();
-
-app.UseForwardedHeaders();
-app.UseRouting();
-if (app.Environment.IsDevelopment())
-{
-    app.UseCookiePolicy(new CookiePolicyOptions
+    if (authConfig != null)
     {
-        MinimumSameSitePolicy = SameSiteMode.Unspecified,
-        OnAppendCookie = opts => opts.CookieOptions.SameSite = SameSiteMode.Unspecified,
-        OnDeleteCookie = opts => opts.CookieOptions.SameSite = SameSiteMode.Unspecified,
-    });
-}
+        app.UseAuthentication();
+        app.UseAuthorization();
+    }
 
-if (authConfig != null)
+    app.UseStaticFiles();
+    app.UseApiProxy();
+    app.MapControllers();
+    app.MapFallbackToFile("index.html");
+
+    app.Run();
+}
+catch (Exception e)
 {
-    app.UseAuthentication();
-    app.UseAuthorization();
+    Log.Fatal(e, "Unhandled exception, shutting down");
 }
-
-app.UseStaticFiles();
-app.UseApiProxy();
-app.MapControllers();
-app.MapFallbackToFile("index.html");
-
-app.Run();
+finally
+{
+    Log.Information("Shutdown complete");
+    Log.CloseAndFlush();
+}

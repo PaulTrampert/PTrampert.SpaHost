762dccb111b51d50c913c96c95ec9df1693ddde7
Paul Trampert <paul.trampert@ptrampert.com>
2022-11-20 21:05:40 -0500

    Add xsrf protection support.

---
+++ PTrampert.SpaHost/Controllers/AntiforgeryController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Antiforgery;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace PTrampert.SpaHost.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    [Authorize]
+    public class AntiforgeryController : ControllerBase
+    {
+        private readonly IAntiforgery antiforgeryService;
+
+        public AntiforgeryController(IAntiforgery antiforgeryService)
+        {
+            this.antiforgeryService = antiforgeryService;
+        }
+
+        public async Task Get()
+        {
+            var tokens = antiforgeryService.GetAndStoreTokens(HttpContext);
+            Response.Cookies.Append("XSRF-TOKEN", tokens.RequestToken!, new CookieOptions
+            {
+                HttpOnly = false,
+                SameSite = SameSiteMode.Strict,
+                IsEssential = true,
+            });
+        }
+    }
+}
--- PTrampert.SpaHost/Program.cs
+++ PTrampert.SpaHost/Program.cs
@@ -1,11 +1,11 @@
-
 using System.IdentityModel.Tokens.Jwt;
+using System.Text.Json;
+using Mcrio.Configuration.Provider.Docker.Secrets;
 using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Authentication.OpenIdConnect;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.DataProtection;
-using System.Text.Json;
-using Mcrio.Configuration.Provider.Docker.Secrets;
+using Microsoft.AspNetCore.Mvc;
 using PTrampert.SpaHost.Configuration;
 using Serilog;
 using StackExchange.Redis;
@@ -22,13 +22,9 @@ try
     {
         var additionalConfigs = Environment.GetEnvironmentVariable("SPAHOST_ADDITIONAL_APPSETTINGS");
         if (additionalConfigs != null)
-        {
             foreach (var file in additionalConfigs.Split(",",
                          StringSplitOptions.TrimEntries | StringSplitOptions.RemoveEmptyEntries))
-            {
                 configurationBuilder.AddJsonFile(file);
-            }
-        }
 
         configurationBuilder.AddDockerSecrets();
         configurationBuilder.AddEnvironmentVariables();
@@ -36,7 +32,7 @@ try
     });
     var config = builder.Configuration;
 
-    builder.Host.UseSerilog((ctx, lc) => 
+    builder.Host.UseSerilog((ctx, lc) =>
         lc.ReadFrom.Configuration(ctx.Configuration));
 
     var authConfig = config.GetSection("AuthConfig")?.Get<AuthConfig>();
@@ -45,8 +41,23 @@ try
 
     JwtSecurityTokenHandler.DefaultInboundClaimTypeMap.Clear();
 
+
     builder.Services.AddOptions<JsonSerializerOptions>()
         .Configure(opts => opts.PropertyNamingPolicy = JsonNamingPolicy.CamelCase);
+
+    if (config.GetValue("Antiforgery:EnableProtection", true))
+    {
+        builder.Services.AddAntiforgery(opts =>
+        {
+            opts.HeaderName = config.GetValue<string>("Antiforgery:HeaderName", "X-XSRF-TOKEN");
+            opts.FormFieldName = config.GetValue<string>("Antiforgery:FieldName", "antiforgeryToken");
+        });
+        builder.Services.AddControllersWithViews(opts =>
+        {
+            opts.Filters.Add<AutoValidateAntiforgeryTokenAttribute>();
+        });
+    }
+
     builder.Services.AddApiProxy(builder.Configuration.GetSection("ApiProxy"));
 
     if (authConfig != null)
@@ -69,14 +80,12 @@ try
             .AddOpenIdConnect(authConfig.OidcConfig.ConfigureOidc);
 
         if (authConfig.RequireForStaticFiles)
-        {
             builder.Services.AddAuthorization(opts =>
             {
                 opts.FallbackPolicy = new AuthorizationPolicyBuilder()
                     .RequireAuthenticatedUser()
                     .Build();
             });
-        }
     }
 
     builder.Services.Configure<ForwardedHeadersOptions>(fhConfig.ConfigureForwardedHeaders);
@@ -88,14 +97,12 @@ try
     app.UseForwardedHeaders();
     app.UseRouting();
     if (app.Environment.IsDevelopment())
-    {
         app.UseCookiePolicy(new CookiePolicyOptions
         {
             MinimumSameSitePolicy = SameSiteMode.Unspecified,
             OnAppendCookie = opts => opts.CookieOptions.SameSite = SameSiteMode.Unspecified,
-            OnDeleteCookie = opts => opts.CookieOptions.SameSite = SameSiteMode.Unspecified,
+            OnDeleteCookie = opts => opts.CookieOptions.SameSite = SameSiteMode.Unspecified
         });
-    }
 
     if (authConfig != null)
     {
